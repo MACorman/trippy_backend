74bfd5720d2af8e91a5e8301ab548562c38cfa89
MACorman <madelinecorman@gmail.com>
2020-02-24 17:53:39 -0500

    added delete controller actions

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -4,4 +4,16 @@ class DestinationsController < ApplicationController
         destinations = Destination.all 
         render json: destinations
     end
+
+    def create
+        destination = Destination.find_or_create_by(destination_params)
+        render json: destination
+    end
+
+    private
+    
+    def destination_params
+        params.require(:destination).permit(:name, :address, :category)
+    end
+
 end

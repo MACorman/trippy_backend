f75256ad129c9f44af5b37bd5f4afa7f19bf1155
MACorman <madelinecorman@gmail.com>
2020-02-25 13:20:08 -0500

    controller actions to create and edit user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,26 @@ class UsersController < ApplicationController
         users = User.all
         render json: users
     end
+
+    def show
+        user = User.find(params[:id])
+        render json: user
+    end
+
+    def create
+        user = User.create(user_params)
+        render json: user
+    end
+
+    def update
+        user = User.find(params[:id])
+        user.update(user_params)
+        render json: user
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password, :image)
+    end 
 end
